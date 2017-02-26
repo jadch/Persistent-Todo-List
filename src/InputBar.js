97461a19a771949f2ac64be44ff5083a515899e3
Jad <jad.chalita@gmail.com>
2017-02-26 12:00:58 +0100

    Setting up TaskList as InputBar state

--- src/InputBar.js
+++ src/InputBar.js
@@ -1,11 +1,25 @@
 const React = require('react')
 
 const InputBar = React.createClass({
+  getInitialState () {
+    return {
+      TaskList: []
+    }
+  },
+  addTask (Task) {
+    const items = this.state.TaskList
+    items.push({text: this._inputElement.value, key: Date.now()})
+    this.setState({
+      TaskList: items
+    })
+    this._inputElement.value = ""
+    Task.preventDefault() /*Preventing default onSubmit POST event behavior*/
+  },
   render () {
     return (
       <div className="InputBar">
-        <form>
-          <input placeholder="Enter a new task"></input>
+        <form onSubmit={this.addTask}>
+          <input ref={(a) => this._inputElement = a} placeholder="Enter a new task"></input>
           <button type="submit">Add</button>
         </form>
       </div>
