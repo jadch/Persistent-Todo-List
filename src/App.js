73125c4536bf44c30129cd57c7185305adc5e677
Jad <jad.chalita@gmail.com>
2017-04-26 16:11:39 +0000

    Added title to the top left of page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
+        <div className='Title'>
+          <h1>To-Do List</h1>
+        </div>
         <InputForm />
         <ListItems />
         <CompleteTasks />
