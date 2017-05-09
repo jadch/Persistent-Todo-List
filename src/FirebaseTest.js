8532e457c8a8bdffcd2d92a16bb06304cd4c9b01
Jad <jad.chalita@gmail.com>
2017-05-09 19:20:46 +0000

    Separated the firebase config file

--- src/FirebaseTest.js
+++ src/FirebaseTest.js
@@ -1,17 +1,6 @@
 const React = require('react')
 const { connector } = require('./store')
-const firebase = require('firebase')
 
-const config = {
-  apiKey: "AIzaSyBtRcXsIDYJ3EDnnQ8tw3uFBognc2XW67Y",
-  authDomain: "todo-list-67a7b.firebaseapp.com",
-  databaseURL: "https://todo-list-67a7b.firebaseio.com",
-  projectId: "todo-list-67a7b",
-  storageBucket: "todo-list-67a7b.appspot.com",
-  messagingSenderId: "486254664276"
-}
-
-const database = firebase.initializeApp(config)
 
 const FirebaseTest = React.createClass({
   render () {
