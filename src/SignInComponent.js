e2e6107314f6b6d6fb26b075782281aa19b1c0d7
Jad ch <jad.chalita@gmail.com>
2017-05-15 23:05:16 +0200

    Implemented sign-in via Google popup

--- src/SignInComponent.js
+++ src/SignInComponent.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { auth, googleAuthProvider } from './firebase.js'
 const { connector } = require('./store')
+
+const SignInAction = () => {
+  console.log('Sign in!')
+  auth.signInWithPopup(googleAuthProvider)
+}
 
 let SignInComponent = React.createClass({
   render () {
     return(
-      <div className='SignInComponent'>
+      <div className='SignInComponent' onClick={SignInAction}>
         <button>
           Sign In
         </button>
