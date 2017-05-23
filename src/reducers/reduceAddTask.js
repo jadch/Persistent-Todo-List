03f4728a82e6e6efc1bde15a6adee89334cb8626
Jad ch <jad.chalita@gmail.com>
2017-05-23 23:25:38 +0200

    reduceAddTask now updates the state in Firebase

--- src/reducers/reduceAddTask.js
+++ src/reducers/reduceAddTask.js
@@ -1,9 +1,23 @@
+import { database } from '../firebase.js'
+
+const TaskList_ref = database.ref('TaskList')
+const EditList_ref = database.ref('EditList')
+const TaskCount_ref = database.ref('/')
+
 const reduceAddTask = (state, action) => {
   let newState = {}
   Object.assign(newState, state)
   newState.TaskList.push(action.value)
   newState.EditList.push(false)
   newState.TaskCount = state.TaskCount + 1
+
+  TaskList_ref.update(newState.TaskList)
+  EditList_ref.update(newState.EditList)
+  TaskCount_ref.update({
+    TaskCount: newState.TaskCount
+  })
+
+
   return newState
 }
 
