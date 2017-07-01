3747817944d97797d1d63a6477c9efb9bc0ac0c0
Jad ch <jad.chalita@gmail.com>
2017-07-01 20:25:39 +0200

    TaskList and EditList now nested under user id node in firebase

--- src/reducers/reduceAddTask.js
+++ src/reducers/reduceAddTask.js
@@ -1,15 +1,19 @@
 import { database } from '../firebase.js'
 
-const TaskList_ref = database.ref('TaskList')
-const EditList_ref = database.ref('EditList')
-const TaskCount_ref = database.ref('/TaskCount')
-
 const reduceAddTask = (state, action) => {
   let newState = {}
   Object.assign(newState, state)
+
+  // Updating local redux state
   newState.TaskList.push(action.value)
   newState.EditList.push(false)
   newState.TaskCount = state.TaskCount + 1
+
+  // Updating firebase
+  const uid = state.currentUser.uid
+  const TaskList_ref = database.ref(uid + '/TaskList')
+  const EditList_ref = database.ref(uid + '/EditList')
+  const TaskCount_ref = database.ref(uid + '/TaskCount')
 
   TaskList_ref.set(newState.TaskList)
   EditList_ref.set(newState.EditList)
