a3fc9ab9591b7676f4d684fac090ab94a8b32940
Jad ch <jad.chalita@gmail.com>
2017-07-25 10:28:51 +0200

    Deleted standalone SignIn and SignOut components

--- src/App.js
+++ src/App.js
@@ -4,23 +4,19 @@ import { connector } from './store.js'
 const InputForm = require('./InputForm')
 const ListItems = require('./ListItems')
 const CompleteTasks = require('./CompleteTasks')
-const SignInComponent = require('./SignInComponent')
-const SignOutComponent = require('./SignOutComponent')
 const AuthButton = require('./AuthButton')
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
+        <AuthButton />
         <div className='Title'>
           <h1>To-Do List</h1>
         </div>
-        <AuthButton />
         <InputForm />
         <ListItems />
         <CompleteTasks />
-        <SignInComponent />
-        <SignOutComponent />
       </div>
     )
   }
